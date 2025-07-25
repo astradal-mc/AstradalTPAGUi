c5e7e6152f00f9b29dfa9e3d4fa0c316d39bc152
Pookachu <ethandanielhunt@gmail.com>
2025-07-25 01:47:18 -0500

    Finished InventoryClickListener

--- src/main/java/net/astradal/astradalTPAGUi/gui/listeners/InventoryClickListener.java
+++ src/main/java/net/astradal/astradalTPAGUi/gui/listeners/InventoryClickListener.java
@@ -1,29 +1,58 @@
 package net.astradal.astradalTPAGUi.gui.listeners;
 
 import net.astradal.astradalTPAGUi.gui.GUI;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class InventoryClickListener implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
 
         Inventory inventory = event.getClickedInventory();
-
-        if (inventory == null || !(inventory.getHolder(false) instanceof GUI gui)) {
-            return;
-        }
+        if (inventory == null || !(inventory.getHolder(false) instanceof GUI gui)) return;
 
         event.setCancelled(true);
 
+        ItemStack clicked = event.getCurrentItem();
+        if (clicked == null || clicked.getType() != Material.PLAYER_HEAD ) return;
+
         Player clicker = (Player) event.getWhoClicked();
+
+        ItemMeta meta = clicked.getItemMeta();
+        if (!(meta instanceof SkullMeta skullMeta)) return;
+
+        NamespacedKey key = new NamespacedKey(gui.plugin, "tpa_target");
+
+        String uuidStr = skullMeta.getPersistentDataContainer().get(key, PersistentDataType.STRING);
+        if (uuidStr == null) {
+            clicker.sendMessage(Component.text("Could not find target player", NamedTextColor.RED));
+            return;
+        }
+
+        UUID uuid = UUID.fromString(uuidStr);
+        Player target = Bukkit.getPlayer(uuid);
+
+        if (target != null && target.isOnline()) {
+            clicker.teleport(target.getLocation());
+            clicker.sendMessage(Component.text("Teleported to " + target.getName(), NamedTextColor.GREEN));
+        } else {
+            clicker.sendMessage(Component.text("That player is no longer online.", NamedTextColor.RED));
+        }
+
     }
 }
